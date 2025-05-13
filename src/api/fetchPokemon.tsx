06f6bbd48ec55c4f6a7c38b51cd72fcf153a3ae9
iris <iriskurien@gmail.com>
2025-05-13 22:04:11 +0530

    (feat) fetch pokemon data

---
+++ src/api/fetchPokemon.tsx
@@ -0,0 +1,46 @@
+import axios from "axios";
+
+export interface PokeType {
+    type: {
+        name: string;
+    }
+}
+
+export interface PokeStats {
+    base_stat: number;
+    stat: {
+        name: string;
+    }
+}
+
+export interface Pokemon{
+    id : number;
+    name: string;
+    sprites: {
+        other: {
+            "official-artwork": {
+                front_default: string;
+            }   
+        }
+    }
+        types: PokeType[]
+        stats: PokeStats[]
+}
+
+export const fetchPokemon = async ():
+Promise<Pokemon[]> => {
+    const limit = 100;
+    const response = await axios.get(
+        `https://pokeapi.co/api/v2/pokemon?limit=${limit}`
+    )
+
+    const results = response.data.results
+
+    const pokeData = await Promise.all(
+        results.map(async (pokemon: {url:string}) => {
+            const response = await axios.get(pokemon.url)
+            return response.data as Pokemon
+        })
+    )
+    return pokeData
+}
