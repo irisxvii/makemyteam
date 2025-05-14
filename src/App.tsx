8220e722674728f80d5431bfde043b800ef3ddc2
iris <iriskurien@gmail.com>
2025-05-14 17:03:12 +0530

    (add) select pokemon on card click

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import { useQuery } from "@tanstack/react-query"
 import { fetchPokemon} from "./api/fetchPokemon"
 import type { Pokemon } from "./api/fetchPokemon"
 import PokeCard from "./components/PokeCard"
+import { useState } from "react"
 
 const App = () => {
   const {data: pokemons} =
@@ -10,12 +11,24 @@ const App = () => {
     queryFn: fetchPokemon,
   })
 
+  const [team, setTeam] = useState<Pokemon[]>([])
+  const addToTeam = (pokemon: Pokemon) => {
+    if (team.length < 6) {
+      setTeam((prev) => [...prev, pokemon])
+    } else {
+      alert("you can only have 6 pokemons in your team")
+    }
+  }
+
   return (
     <div>
       <h1 className="heading">Pick Your Dream Team</h1>
       <div className="poke-grid">
         {pokemons?.map((pokemon) => (
-          <PokeCard key={pokemon.id} pokemon={pokemon} />
+          <PokeCard 
+            key={pokemon.id} 
+            pokemon={pokemon}
+            onClick={addToTeam} />
         ))}
 
       </div>
--- src/components/PokeCard.tsx
+++ src/components/PokeCard.tsx
@@ -2,11 +2,12 @@ import type { Pokemon } from "../api/fetchPokemon"
 
 interface Props {
     pokemon: Pokemon;
+    onClick: (pokemon: Pokemon) => void;
 }
 
-const PokeCard: React.FC<Props> = ({ pokemon }) => {
+const PokeCard: React.FC<Props> = ({ pokemon, onClick }) => {
     return (
-        <div className="poke-card">
+        <div className="poke-card" onClick={() => onClick(pokemon)}>
             <img src={pokemon.sprites.other['official-artwork'].front_default}
             alt={pokemon.name} />
         <h3>{pokemon.name}</h3>
