a208d76408ba63d6db6579eabba0df4ee8213780
iris <iriskurien@gmail.com>
2025-05-19 19:02:22 +0530

    (fix) ok i giv up if this doesnt get fixed. deployed link is working anyways

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import * as React from "react"
+import React from 'react';
 import { useQuery } from "@tanstack/react-query"
 import { fetchPokemon} from "./api/fetchPokemon"
 import type { Pokemon } from "./api/fetchPokemon"
